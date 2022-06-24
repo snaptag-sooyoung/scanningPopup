deeec9acedd0a1f441b75a227a69270cfa3b5e61
sooyoung <sooyoung159@snaptag.co.kr>
2022-06-24 15:45:56 +0900

    유튜브 영상 추가

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,14 +1,25 @@
+import YouTube from "react-youtube";
 import "./modal.css";
 
 const Modal = (props) => {
   const onCheckHandler = (event) => {
     if (event.target.checked) props.modal();
   };
+
+  const opts = {
+    height: "200px",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
   return (
     <div className="backdrop">
       <div className="modal">
-        <header className="header">광고창</header>
-        <div className="content">광고</div>
+        <header className="header">롯데칠성</header>
+        <div className="content">
+          <YouTube opts={opts} videoId="759tymJSwJU" />
+        </div>
         <footer className="footer">
           <label>
             <input type="checkbox" onChange={onCheckHandler} /> 더 이상 광고 보지 않기
